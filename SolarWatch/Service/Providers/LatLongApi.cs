3237365d5b872d0c34e732ef664708ab3b8273e6
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-24 23:04:58 +0100

    Create api providers for location and weather api

--- SolarWatch/Service/Providers/LatLongApi.cs
+++ SolarWatch/Service/Providers/LatLongApi.cs
@@ -1,6 +1,22 @@
 namespace SolarWatch.Service.Providers;
 
-public class LatLongApi
+public class LatLongApi : ILocationDataProvider
 {
+    private readonly ILogger _logger;
     
+    public LatLongApi(ILogger<LatLongApi> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<string> GetCityLocationData(string city)
+    {
+        var apiKey = "02f54b1400874d6c45ad6a43c9a28fcf";
+        var url = $"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={apiKey}";
+        using var client = new HttpClient();
+
+        _logger.LogInformation($"Calling OpenWeather API with url: {url}", url);
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
--- SolarWatch/Service/Providers/SunsetSunriseApi.cs
+++ SolarWatch/Service/Providers/SunsetSunriseApi.cs
@@ -1,6 +1,21 @@
 namespace SolarWatch.Service.Providers;
 
-public class SunsetSunriseApi
+public class SunsetSunriseApi : IWeatherDataProvider
 {
+    private readonly ILogger _logger;
     
+    public SunsetSunriseApi(ILogger<SunsetSunriseApi> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<string> GetSunsetSunriseData(double lat, double lon, DateTime date)
+    {
+        var url = $"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&tzid=Europe/Budapest&date={date:yyyy-MM-dd}";
+        using var client = new HttpClient();
+
+        _logger.LogInformation($"Calling OpenWeather API with url: {url}", url);
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
