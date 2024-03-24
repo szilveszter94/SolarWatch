7909c6c282fa118629486428219745f2af91c7f3
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-24 23:05:37 +0100

    Add processors to process location and weather data

--- SolarWatch/Service/Processors/LocationDataProcessor.cs
+++ SolarWatch/Service/Processors/LocationDataProcessor.cs
@@ -1,6 +1,17 @@
+using System.Text.Json;
+using SolarWatch.Model;
+
 namespace SolarWatch.Service.Processors;
 
-public class LocationDataProcessor
+public class LocationDataProcessor : ILocationDataProcessor
 {
-    
+    public LocationData Process(string data)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        string city = json.RootElement[0].GetProperty("name").GetString() ?? "";
+        double lat = json.RootElement[0].GetProperty("lat").GetDouble();
+        double lon = json.RootElement[0].GetProperty("lon").GetDouble();
+
+        return new LocationData ( 0, city, lat, lon );
+    }
 }
--- SolarWatch/Service/Processors/SunsetSunriseDataProcessor.cs
+++ SolarWatch/Service/Processors/SunsetSunriseDataProcessor.cs
@@ -1,6 +1,17 @@
+using System.Text.Json;
+using SolarWatch.Model;
+
 namespace SolarWatch.Service.Processors;
 
-public class SunsetSunriseDataProcessor
+public class SunsetSunriseDataProcessor : ISunsetSunriseDataProcessor
 {
-    
+    public SunsetSunriseData Process(string data)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement results = json.RootElement.GetProperty("results");
+        string sunrise = results.GetProperty("sunrise").GetString() ?? "";
+        string sunset = results.GetProperty("sunset").GetString() ?? "";
+
+        return new SunsetSunriseData(sunrise, sunset);
+    }
 }
