8e7105d66aecb1cd11512ba25fb2609e8e390a0f
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-25 21:08:55 +0100

    Refactor the context, create user context

--- SolarWatch/Context/SolarWatchContext.cs
+++ SolarWatch/Context/SolarWatchContext.cs
@@ -1,33 +1,33 @@
-using DotNetEnv;
-using Microsoft.EntityFrameworkCore;
-using SolarWatch.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SolarWatch.Context;
 
-public class SolarWatchContext : DbContext
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+public class SolarWatchContext : IdentityDbContext<IdentityUser, IdentityRole, string>
 {
     public DbSet<LocationData> LocationDatas { get; set; }
     public DbSet<CityInformation> CityInformations { get; set; }
     
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public SolarWatchContext(DbContextOptions<SolarWatchContext> options) : base(options)
     {
-        Env.Load();
-        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-        optionsBuilder.UseSqlServer(
-            connectionString);
     }
     
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
+        
         builder.Entity<LocationData>()
             .HasIndex(u => u.City)
             .IsUnique();
     
         builder.Entity<LocationData>()
             .HasData(
-                new LocationData { City = "London", Lat = 51.509865, Lon = -0.118092 },
-                new LocationData { City = "Budapest", Lat = 47.497913, Lon = 19.040236 },
-                new LocationData { City = "Paris", Lat = 48.864716, Lon = 2.349014 }
+                new LocationData { Id = 1, City = "London", Lat = 51.509865, Lon = -0.118092 },
+                new LocationData { Id = 2, City = "Budapest", Lat = 47.497913, Lon = 19.040236 },
+                new LocationData { Id = 3, City = "Paris", Lat = 48.864716, Lon = 2.349014 }
             );
     }
 }
