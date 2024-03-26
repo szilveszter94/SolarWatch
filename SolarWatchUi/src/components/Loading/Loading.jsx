9cc0b329efcc2f883db5d966a5c709a30fe31eda
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-26 01:09:09 +0100

    Add loading page and loading gif

---
+++ SolarWatchUi/src/components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+const Loading = () => {
+    return (
+      <div className="container mt-5">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-md-6 text-center">
+            <img className="img-fluid w-50" src="../../assets/loading.gif" alt="Loading..." />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default Loading
