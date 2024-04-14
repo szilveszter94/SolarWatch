b5bd2f895af8239b14566ce3577196b64a574d9e
Szilveszter <s.szilveszter1994@gmail.com>
2024-04-14 20:14:54 +0200

    fix loading page image url

--- SolarWatchUi/src/components/Loading/Loading.jsx
+++ SolarWatchUi/src/components/Loading/Loading.jsx
@@ -1,13 +1,15 @@
+import image from "../../assets/loading.gif";
+
 const Loading = () => {
-    return (
-      <div className="container mt-5">
-        <div className="row d-flex justify-content-center align-items-center">
-          <div className="col-md-6 text-center">
-            <img className="img-fluid w-50" src="../../assets/loading.gif" alt="Loading..." />
-          </div>
+  return (
+    <div className="container mt-5">
+      <div className="row d-flex justify-content-center align-items-center">
+        <div className="col-md-6 text-center">
+          <img className="img-fluid w-50" src={image} alt="Loading..." />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
